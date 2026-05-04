7f56d72901b3ba707e644a13a676d43f672d772f
analui7 <analui.cp@outlook.com>
2026-05-04 10:21:48 -0300

    sesao08

---
+++ src/sessao8/exercicio01/Main.java
@@ -0,0 +1,26 @@
+package sessao8.exercicio01;
+
+import sessao8.exercio01.entities.Rectangle;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner leia = new Scanner(System.in);
+
+        Rectangle retangulo = new Rectangle();
+
+        System.out.println("Enter ractangle width and height: ");
+        retangulo.width = leia.nextDouble();
+        retangulo.height = leia.nextDouble();
+
+        System.out.println("AREA = "+ String.format("%.2f", retangulo.area()));
+        System.out.println("PERIMETER = "+ String.format("%.2f", retangulo.perimeter()));
+        System.out.println("DIAGONAL = "+ String.format("%.2f", retangulo.diagonal()));
+
+    }
+
+}
