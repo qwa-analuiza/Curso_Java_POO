44efd4d09d14aed5c0d65ed0aabcd1c56e5a7a85
analui7 <analui.cp@outlook.com>
2026-05-26 12:07:43 -0300

    inserindo interfaces

---
+++ JavaJDBC/jdbc-dao-demo/src/model/dao/SellerDao.java
@@ -0,0 +1,12 @@
+package model.dao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller obj);
+    void Update(Seller obj);
+    void deletById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
