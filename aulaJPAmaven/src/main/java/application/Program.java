8173a4207f284bdd1594cbb17f4b604a3284f181
analui7 <analui.cp@outlook.com>
2026-05-28 14:16:16 -0300

    Intrudução a JPA

---
+++ aulaJPAmaven/src/main/java/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+import dominio.Pessoa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Program {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        Pessoa p = em.find(Pessoa.class, 2);
+
+        System.out.println("Done!" + p);
+        em.close();
+        emf.close();
+    }
+}
