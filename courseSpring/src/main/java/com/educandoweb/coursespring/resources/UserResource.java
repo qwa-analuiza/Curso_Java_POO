ec72210c0b22ce64898d9ddfa444ffcf1ef623c4
analui7 <analui.cp@outlook.com>
2026-06-01 11:12:21 -0300

    update implementation

--- courseSpring/src/main/java/com/educandoweb/coursespring/resources/UserResource.java
+++ courseSpring/src/main/java/com/educandoweb/coursespring/resources/UserResource.java
@@ -25,22 +25,28 @@ public class UserResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Long id){
+    public ResponseEntity<User> findById(@PathVariable Long id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
     @PostMapping
-    public ResponseEntity<User> insert(@RequestBody User obj){
+    public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(";{id}").buildAndExpand(obj.getId()).toUri() ;
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(";{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
 
-    @DeleteMapping( value = "/{id}")
-    public ResponseEntity <Void> delete(@PathVariable Long id){
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- courseSpring/src/main/java/com/educandoweb/coursespring/services/UserServices.java
+++ courseSpring/src/main/java/com/educandoweb/coursespring/services/UserServices.java
@@ -2,6 +2,7 @@ package com.educandoweb.coursespring.services;
 
 import com.educandoweb.coursespring.entities.User;
 import com.educandoweb.coursespring.repositories.UserRepository;
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,18 @@ public class UserServices {
 
     public void delete(Long id){
      repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return  repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj){
+       entity.setName(obj.getName());
+       entity.setEmail(obj.getEmail());
+       entity.setPhone(obj.getPhone());
     }
 
 }
