652f1e5e24bbf559ed0c9bd27c7ca30802d37882
analui7 <analui.cp@outlook.com>
2026-06-03 10:41:55 -0300

    Chess layer and printing the board

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -27,4 +27,13 @@ public class Board {
     public void setRows(int rows) {
         this.rows = rows;
     }
+
+    public  Piece piece(int row, int columns){
+        return pieces [row][columns];
+    }
+
+    public Piece piece(Position position){
+        return pieces[position.getRow()][position.getColum()];
+    }
+
 }
