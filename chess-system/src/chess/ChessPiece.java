705cb023513a1bb892f200f05481a638dae033ca
analui7 <analui.cp@outlook.com>
2026-06-08 13:35:56 -0300

    contagem movimento das peças

--- chess-system/src/chess/ChessPiece.java
+++ chess-system/src/chess/ChessPiece.java
@@ -7,23 +7,35 @@ import boardgame.Position;
 public abstract class ChessPiece extends Piece {
 
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
         this.color = color;
     }
 
+    public int getMoveCount() {
+        return moveCount;
+    }
+
     public Color getColor() {
         return color;
     }
 
-    public ChessPosition getChessPosition(){
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
+    public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
-
     protected boolean isThereOpponentPiece(Position position) {
-        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
 }
