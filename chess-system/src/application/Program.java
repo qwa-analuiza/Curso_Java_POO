c51d2de090c4dc6f507b62c5c6ad2b6389dd6ea5
analui7 <analui.cp@outlook.com>
2026-06-08 08:25:15 -0300

    commit chess

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,12 +1,41 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
-        ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner leia = new Scanner(System.in);
 
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true){
+            try {
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(leia);
+
+
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(leia);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                leia.nextLine();
+            }
+            catch(InputMismatchException e) {
+                System.out.println(e.getMessage());
+                leia.nextLine();
+            }
+        }
     }
 }
--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -44,6 +44,18 @@ public class Board {
         pieces[position.getRow()][position.getColum()] = piece;
         piece.position = position;
     }
+    public Piece removePiece(Position position){
+        if (!positionExists(position)) {
+            throw new BoardException("Posição não está no tabuleiro");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColum()] = null;
+        return  aux;
+    }
 
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -59,5 +71,6 @@ public class Board {
         }
         return piece(position) != null;
     }
+
 
 }
