12e8f90bf226e156382c9f5b27d8fa4a93bd6a47
analui7 <analui.cp@outlook.com>
2026-06-08 14:58:28 -0300

    Queen and bug Pawn

--- chess-system/src/chess/pieces/Pawn.java
+++ chess-system/src/chess/pieces/Pawn.java
@@ -42,7 +42,7 @@ public class Pawn extends ChessPiece {
                 mat[p.getRow()][p.getColum()] = true;
             }
             p.setValues(position.getRow() + 2, position.getColum());
-            Position p2 = new Position(position.getRow() - 1, position.getColum());
+            Position p2 = new Position(position.getRow() + 1, position.getColum());
             if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
                 mat[p.getRow()][p.getColum()] = true;
             }
