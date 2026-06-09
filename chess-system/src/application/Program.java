c27a5792ee9eaed12a43cb063268782c737d389d
analui7 <analui.cp@outlook.com>
2026-06-09 11:23:44 -0300

    Fix - beter treatment to promotion

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -40,7 +40,12 @@ public class Program {
                 }
                 if (chessMatch.getPromoted() != null) {
                     System.out.println("Digite a peça pra ser promovida (B/N/R/Q):");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.println("Valor inválido! "+ "Digite a peça pra ser promovida (B/N/R/Q):");
+                         type = sc.nextLine().toUpperCase();
+                    }
                     chessMatch.replacePromotedPiece(type);
                 }
             } catch (ChessException | InputMismatchException e) {
