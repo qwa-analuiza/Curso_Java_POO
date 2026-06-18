94a18f7655f9dbf59b62b83fe4907bd36ea75b72
Ana Luiza de Oliveira <analui.cp@outlook.com>
2026-06-18 13:26:59 -0300

    Obtendo um post por id

---
+++ workshopmongo/src/main/java/resources/PostResource.java
@@ -0,0 +1,21 @@
+package resources;
+
+import domain.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
