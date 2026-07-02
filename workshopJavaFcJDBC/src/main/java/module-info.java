77ccb49bad618f4051dd186930675a65e5fc5e2c
Ana Luiza de Oliveira <analui.cp@outlook.com>
2026-07-02 11:42:04 -0300

    main view controller

--- workshopJavaFcJDBC/src/main/java/module-info.java
+++ workshopJavaFcJDBC/src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.educandoweb.workshopjavafcjdbc {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens com.educandoweb.workshopjavafcjdbc.gui to javafx.fxml;
+
     exports com.educandoweb.workshopjavafcjdbc;
-    opens com.educandoweb.workshopjavafcjdbc to javafx.fxml;
 }
